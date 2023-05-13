793f5da377af25a58af39216c3eb5efd4c87653c
didiersnake <didier.djakoua@gmail.com>
2023-05-13 23:06:39 +0100

    countries list complete

--- src/feature/CountryList.js
+++ src/feature/CountryList.js
@@ -9,19 +9,41 @@ const CountryList = () => {
     const countryCards = data.map((item) => {
         const {name, flags, population, region, capital} = item
         return (
-          <div key={name.common} className="border-2 border-gray-200 dark:border-gray-800">
-                <div><img className='' src={flags.svg} alt={name.common} /></div>            
-                <div>{name.common}</div>            
-                <div>Population: {population}</div>            
-                <div>Region: {region}</div>            
-                <div>Capital: {capital}</div>            
+          <div
+            key={name.common}
+            className="border-2 dark:border-4 shadow-sm dark:bg-[#2B3743] dark:border-gray-800 text-sm text-black dark:text-slate-300"
+          >
+            <div>
+              <img
+                className="h-[45%] w-full"
+                src={flags.png}
+                alt={name.common}
+              />
+            </div>
+            <div className="px-4 pb-8">
+              <div className="py-3 text-base text-black dark:text-white">
+                {name.common}
+              </div>
+              <div className="flex gap-1">
+                Population :
+                <p className="dark:text-gray-400 text-slate-500 ">
+                  {population.toLocaleString("en-US")}
+                </p>
+              </div>
+              <div className="flex gap-1">
+                Region : <p className="dark:text-gray-400 text-slate-500 ">{region}</p>
+              </div>
+              <div className="flex gap-1">
+                Capital : <p className="dark:text-gray-400 text-slate-500 ">{capital}</p>
+              </div>
+            </div>
           </div>
         );
     })
   return (
     <div className="h-full dark:bg-[#202D36] dark:text-white mx-auto md:px-16 px-6 font-medium bg-slate-100 sm:text-sm md:text-base">
       <div className="py-6">Hello</div>
-      <div className="lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-2 sm:gap-5 grid max-w-5xl mx-auto">
+      <div className="lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-2 sm:gap-6 gap-8 grid max-w-5xl sm:px-0 px-12 mx-auto">
         {countryCards}
       </div>
     </div>
